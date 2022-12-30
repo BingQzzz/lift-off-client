db2ba0500a2affaac336e09e5aff7ed261baa4c9
BingQ <bingbing13@163.com>
2022-12-30 15:53:52 +0800

    update uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'lift-off-server-production.up.railway.app',
+  uri: 'https://lift-off-server-production.up.railway.app/',
   cache: new InMemoryCache(),
 });
 
